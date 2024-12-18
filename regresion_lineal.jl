6cd13527ed6365be359b9ceb936713cd8db0c055
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2024-12-18 13:32:36 +0100

    Ajuste polinomial interactivo.

--- regresion_lineal.jl
+++ regresion_lineal.jl
@@ -4,6 +4,18 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
 # ╔═╡ 6f6339a7-9b4d-4272-94f7-9234d3d3be41
 begin
 	using CSV
@@ -926,6 +938,26 @@ rms(fit3.(data[:, :weight]), data[:, :height])
 
 # ╔═╡ eff7c325-285b-4b0a-9136-6a78f3b86376
 rms(fit15.(data[:, :weight]), data[:, :height])
+
+# ╔═╡ e982d0a1-3469-48d5-a286-28fc2f88f714
+@bind grado Slider(2:10, show_value=true)
+
+# ╔═╡ 4f59d4d0-8c87-41d6-b313-1cdc368a5120
+function dibuja_ajuste(grado::Int)
+	fitx = Polynomials.fit(data[:,:weight], data[:,:height], grado)
+	scatter(data[:, :weight], data[:, :height], xlabel="weight", ylabel="height", label="Datos", legend=false, ylim=(50, 200))
+	plot!(fitx, extrema(data[:, :weight])..., width=3)
+end
+
+# ╔═╡ 0ad863f8-3479-4da5-92b2-7bdc2f4044d5
+dibuja_ajuste(grado)
+
+# ╔═╡ 0671d1d1-5ac9-41a6-9526-3bc8b91cf31a
+begin
+	fitx = Polynomials.fit(data[:,:weight], data[:,:height], grado)
+	scatter(data[:, :weight], data[:, :height], xlabel="weight", ylabel="height", label="Datos", legend=false, ylim=(50, 200))
+	plot!(fitx, extrema(data[:, :weight])..., width=3)
+end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -3351,5 +3383,9 @@ version = "1.4.1+1"
 # ╠═6bd8f588-cc5d-4c78-a891-efc47133a95f
 # ╠═2784ecd3-8303-46ee-84dc-463780a3a7c5
 # ╠═eff7c325-285b-4b0a-9136-6a78f3b86376
+# ╠═e982d0a1-3469-48d5-a286-28fc2f88f714
+# ╠═4f59d4d0-8c87-41d6-b313-1cdc368a5120
+# ╠═0ad863f8-3479-4da5-92b2-7bdc2f4044d5
+# ╠═0671d1d1-5ac9-41a6-9526-3bc8b91cf31a
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
