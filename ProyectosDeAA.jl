e1d4904179db1f4685d828cf2e7dcf04706347a9
oscarbelmon <oscar.belmonte@uji.es>
2025-05-08 09:29:57 +0200

    Primera versión de Proyectos de Aprendizaje Automático.

--- ProyectosDeAA.jl
+++ ProyectosDeAA.jl
@@ -17,13 +17,203 @@ TableOfContents(title="Contenidos", depth=1)
 
 # ╔═╡ 2ba8182e-7607-4516-8c65-7f2b8362672f
 md"""
-# Proyectos de AprendizajeAutomático
+# Proyectos de Aprendizaje Automático
 
 Óscar Belmonte Fernández - IR2130 Aprendizaje Automático
 
 Grado en Inteligencia Robótica - Universitat Jaume I (UJI)
 
 ![](https://ujiapps.uji.es/ade/rest/storage/63c07717-5208-4240-b688-aa6ff558b466?guest=true)
+"""
+
+# ╔═╡ 39a5d438-5034-49d6-9256-9fbe43fc9e01
+md"""
+## Introducción
+
+Un proyecto de AA necesita de cierta organización previa antes de su inicio. 
+En particular, el uso de los datos debe ser claramente establecido.
+
+El desarrollo de este tipo de proyectos es una fase continua de prueba y error, 
+siempre hay espacio suficiente para probar nuevas idea.
+
+Como en todo proyecto, es muy importante ir documentando todo el trabajo, así 
+como las conclusiones que se van obteniendo.
+"""
+
+# ╔═╡ 276f4250-bd8c-408c-9cb0-0493f5f7b3fe
+md"""
+## Objetivos de aprendizaje
+
+* Organizar las tareas necesarias para llevar a cabo un proyecto de AA.
+* Construir un modelo de AA y valorar lo consecución de los objetivos.
+* Planear posibles mejoras a partir de los resultados obtenidos.
+"""
+
+# ╔═╡ 67b9cdd1-32b3-431b-9fe4-88fc39057eb0
+md"""
+## Referencias
+
+* Hands-on machine learning with Scikit-learn, Keras and TensorFlow. Capítulo 2 y Apéndice A.
+"""
+
+# ╔═╡ 27f48568-a11b-43a0-bc3a-a2306d3c9963
+md"""
+# Definir el problema y tener una imagen del conjunto
+"""
+
+# ╔═╡ 1eff8fdd-86c1-4ffc-ad78-cce43ee6a786
+md"""
+## Definir el problema y tener una imagen del conjunto
+
+1. Definir los objetivos de la solución que se plantea.
+1. Decidir qué tipo de aproximación es la más adecuada: aprendizaje supervisado, no supervisado, por refuerzo, tarea de clasificación o regresión, etc.
+1. Decidir la métrica de evaluación que se va a emplear para validar el modelo.
+1. Contrastar con soluciones ya existentes.
+1. ¿Se puede contar con la colaboración de expertos?
+"""
+
+# ╔═╡ f8771a01-7018-4ae0-a6e6-1d21c994e9a9
+md"""
+# Obtener los datos
+"""
+
+# ╔═╡ 72a9bc7c-bb05-4814-97d6-90fb17bb37bb
+md"""
+## Obtener los datos
+
+1. ¿Existen datos o hay que adquirirlos?
+1. Si los datos existen, ¿hay documentación sobre ellos? Por ejemplo, descripción y tipo de los campos, cómo se han obtenido, posibles errores en las mediciones, etc.
+1. Comprobar los temas legales sobre los datos.
+1. Obtener los datos y gestionar el acceso.
+"""
+
+# ╔═╡ 856c2092-93ac-4c77-8455-d838fd74ab62
+md"""
+# Explorar los datos para conocerlos mejor
+"""
+
+# ╔═╡ de706874-4566-477a-bcfa-f4145ea20cdb
+md"""
+## Explorar los datos para conocerlos mejor
+
+1. Si el volumen de los datos es muy grande, tomar una muestra de ellos para su inspección.
+1. Realizar un análisis estadístico descriptivo de los datos.
+1. Visualizar los datos.
+1. Analizar las correlaciones entre las características.
+1. Registrar todas las ideas que vayan surgiendo.
+"""
+
+# ╔═╡ ecf46d82-359d-4e54-ac55-e5748a9ef97e
+md"""
+# Preparar lo datos para que muestren los patrones
+"""
+
+# ╔═╡ 27d4223e-66cb-4136-9d37-bb9cc704a0da
+md"""
+## Preparar lo datos para que muestren los patrones
+
+1. Limpiar los datos si es necesario.
+1. Seleccionar/eliminar las características interesantes.
+1. Escalar los datos si es necesario.
+
+Automatizar todos los proceso para que sea sencillo repetirlos si fuese 
+necesario.
+"""
+
+# ╔═╡ 7bef6ea7-48f6-4491-9b16-e08a5597c101
+md"""
+# Identificar los algoritmos prometedores
+"""
+
+# ╔═╡ 7cdff43a-666f-4704-9ece-dbc81d0ab0c2
+md"""
+## Identificar los algoritmos prometedores
+
+1. Seleccionar los algoritmos más interesantes para el problema en cuestión.
+1. Hacer pruebas rápidas para cada uno de los algoritmos seleccionados.
+1. Comprobar si se cumplen las condiciones *ideales* para los algoritmos seleccionados.
+1. Aplicar la/las métricas seleccionadas y registrar los resultados.
+"""
+
+# ╔═╡ eeb73f9a-c310-491c-8d11-3e4954302d59
+md"""
+## Identificar los algoritmos prometedores
+
+5. Añadir/eliminar características y repetir las pruebas.
+1. Seleccionar los algoritmos que dan mejores resultados para la siguiente fase.
+
+De nuevo, automatizar todos los proceso para que sea sencillo repetirlos si 
+fuese necesario.
+"""
+
+# ╔═╡ d119b883-aaa4-48c4-ab72-1c40913a04ce
+md"""
+# Ajustar los modelos y combinarlos para obtener una solución
+"""
+
+# ╔═╡ bb0fa793-9662-4926-9eef-077d22963c15
+md"""
+## Ajustar los modelos y combinarlos para obtener una solución
+
+1. Intentar ajustar cada uno de los modelos seleccionados.
+1. Probar un ensamblado con todos los modelos seleccionados.
+1. Registrar todas las conclusiones obtenidas.
+"""
+
+# ╔═╡ 5842c918-9ebf-4903-be1c-7f5377b66002
+md"""
+# Presentar la solución
+"""
+
+# ╔═╡ 2754be9e-f58b-4921-a3dc-d43eed844362
+md"""
+## Presentar la solución
+
+1. Recopilar todas las conclusiones, ideas y detalles que se han ido registrando durante el desarrollo del proyecto.
+1. Hacer una presentación de los resultados obtenidos.
+1. Realizar una crítica del trabajo realizado.
+1. Plantear posibles mejoras.
+"""
+
+# ╔═╡ 9c99a746-9534-477c-8f73-8a5cf4f94a78
+md"""
+# Poner en marcha el proyecto y mejorarlo continuamente
+"""
+
+# ╔═╡ a1f64f7d-eedb-4ac3-9ceb-e8b751a561c9
+md"""
+## Poner en marcha el proyecto y mejorarlo continuamente
+
+1. Lanzar el proyecto y monitorizarlo a lo largo del tiempo.
+1. Revisar los datos de monitorización.
+1. Detectar posibles fallos del modelo en producción.
+1. Documentar posibles mejoras para una nueva iteración del proyecto.
+"""
+
+# ╔═╡ 2532887c-9182-4167-8ee8-6d3d5cf486e5
+md"""
+# Resumen
+"""
+
+# ╔═╡ 1160e27a-611a-4868-8cd3-77fe30fa4edd
+md"""
+## Resumen
+
+- Tener una estrategia para el desarrollo de un proyecto de AA es fundamental para alcanzar el éxito en su desarrollo.
+- Es muy importante *familiarizarse* con los datos, así como con el contexto del problema.
+- Hacer una exploración inicial de los datos permite *familiarizarse* con ellos.
+- Si existe la posibilidad, hablar con los especialistas en la materia para aclara cualquier duda.
+"""
+
+# ╔═╡ 8eb3ddb5-8ccd-446e-9e1d-bc1150e3f8d9
+md"""
+## Resumen
+
+- Probar varios modelos antes de decidirse por uno.
+- No realizar una ajuste de parámetros temprano porque puede sesgar la selección del modelo.
+- Si es posible, combinar más de una modelo en la solución final.
+- Anotar todas la ideas que vayan surgiendo.
+- Cuidar la presentación de los resultados.
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -321,5 +511,28 @@ version = "17.4.0+2"
 # ╠═0a611a39-2f34-4190-84ea-36791341ed75
 # ╠═8211d6e7-2da7-42dc-a03e-f7e8827d0bd4
 # ╠═2ba8182e-7607-4516-8c65-7f2b8362672f
+# ╠═39a5d438-5034-49d6-9256-9fbe43fc9e01
+# ╠═276f4250-bd8c-408c-9cb0-0493f5f7b3fe
+# ╠═67b9cdd1-32b3-431b-9fe4-88fc39057eb0
+# ╠═27f48568-a11b-43a0-bc3a-a2306d3c9963
+# ╠═1eff8fdd-86c1-4ffc-ad78-cce43ee6a786
+# ╠═f8771a01-7018-4ae0-a6e6-1d21c994e9a9
+# ╠═72a9bc7c-bb05-4814-97d6-90fb17bb37bb
+# ╠═856c2092-93ac-4c77-8455-d838fd74ab62
+# ╠═de706874-4566-477a-bcfa-f4145ea20cdb
+# ╠═ecf46d82-359d-4e54-ac55-e5748a9ef97e
+# ╠═27d4223e-66cb-4136-9d37-bb9cc704a0da
+# ╠═7bef6ea7-48f6-4491-9b16-e08a5597c101
+# ╠═7cdff43a-666f-4704-9ece-dbc81d0ab0c2
+# ╠═eeb73f9a-c310-491c-8d11-3e4954302d59
+# ╠═d119b883-aaa4-48c4-ab72-1c40913a04ce
+# ╠═bb0fa793-9662-4926-9eef-077d22963c15
+# ╠═5842c918-9ebf-4903-be1c-7f5377b66002
+# ╠═2754be9e-f58b-4921-a3dc-d43eed844362
+# ╠═9c99a746-9534-477c-8f73-8a5cf4f94a78
+# ╠═a1f64f7d-eedb-4ac3-9ceb-e8b751a561c9
+# ╠═2532887c-9182-4167-8ee8-6d3d5cf486e5
+# ╠═1160e27a-611a-4868-8cd3-77fe30fa4edd
+# ╠═8eb3ddb5-8ccd-446e-9e1d-bc1150e3f8d9
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
