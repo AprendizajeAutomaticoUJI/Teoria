9d16a4c3591795c176e2825772f9c3486454042a
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2025-05-19 14:50:41 +0200

    Avanzando con las redes covolucionales.

---
+++ redes_convolucionales.jl
@@ -0,0 +1,478 @@
+### A Pluto.jl notebook ###
+# v0.20.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 49bc4ae1-cc37-4de1-8616-d5bd2cc740ad
+using PlutoUI
+
+# ╔═╡ 17163d32-2fd3-11f0-053d-7b01c1dc1e5d
+# html"""
+# <link rel="stylesheet" type="text/css" href="https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/mi_estilo.css" media="screen" />
+# """
+
+# ╔═╡ 98925bf9-6cfa-4046-b0c7-de0a0ad286ad
+TableOfContents(title="Contenidos", depth=1)
+
+# ╔═╡ bc7e1700-5d03-4493-84d7-b96a7da32d1b
+md"""
+# Redes neuronales convolucionales
+
+Óscar Belmonte Fernández - IR2130 Aprendizaje Automático
+
+Grado en Inteligencia Robótica - Universitat Jaume I (UJI)
+
+![](https://ujiapps.uji.es/ade/rest/storage/63c07717-5208-4240-b688-aa6ff558b466?guest=true)
+"""
+
+# ╔═╡ c2db677d-1029-47ff-ac13-309d586ecc69
+md"""
+## Introducción
+
+* Las redes neuronales convolucionales están inspiradas en cómo funciona la visión humana.
+* Las redes convolucionales trabajan con imágenes, aunque se pueden utilizar en otros ámbitos.
+* Las redes convolucionales son capaces de «reconocer» la relación entreregiones de una imagen, también temporales si lo que se analiza es unasecuencia de vídeo.
+"""
+
+# ╔═╡ d8736fb8-a6a9-4e4d-bd28-509b2fdb6336
+md"""
+## Objetivos de aprendizaje
+
+* Resumir en qué consisten las principales tareas de la visión por computador.
+* Definir las bases de las redes neuronales convolucionales: convoluciones y pooling.
+* Construir una red neuronal convolucional utilizando Keras y Tensorflow.
+* Valorar si existe sobreajuste durante el proceso de entrenamiento.
+* Describir algunas de las arquitecturas de redes neuronales convolucionales más importantes.
+"""
+
+# ╔═╡ ce478b27-9250-41ff-898b-c887aa166ed3
+md"""
+## Objetivos de aprendizaje
+
+* Utilizar la arquitectura YOLO para tareas de visión por computador.
+* Resumir en qué consiste la transferencia de aprendizaje.
+* Construir un modelo utilizando transferencia de aprendizaje.
+"""
+
+# ╔═╡ 7900f0f7-3ec0-434e-a6a8-2b6f381d3328
+md"""
+## Referencias
+
+1. [Deep Learning](http://www.deeplearningbook.org), Ian Goodfellow, Joshua Bengio and Aaron Courbille.
+1. [Dive into deep learning](https://d2l.ai/index.html), Aston Zhang et al.
+"""
+
+# ╔═╡ a0bbbc75-a852-47cf-88d5-982d03eb5c06
+md"""
+# Redes neuronales en visión por ordenador
+"""
+
+# ╔═╡ 50a1c924-0a84-4cf0-99b7-476cb4b4afe7
+md"""
+## Introducción
+
+La visión por computador es una disciplina con una gran trayectoria en las Ciencias de la Computación.
+"""
+
+# ╔═╡ 8bdb7e0f-dece-4b09-a4e1-f32ee3f7601f
+Resource(
+	"https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/computer_vision.jpg",
+	:alt => "Computer Vision Book",
+	:width => 600
+)
+
+# ╔═╡ 0dd08d60-2b9d-41d0-bcc8-927853f7c962
+md"""
+## Introducción
+
+Utilizar redes densas en visión por computador es impracticable. El tamaño de 
+una imagen, en píxeles, determina el número de neuronas en la capa de entrada 
+y posteriores.
+
+El número de parámetros que es necesario entrenar es muy elevado.
+"""
+
+# ╔═╡ 799ac9d4-2151-4474-a98e-1476e8bb1033
+md"""
+## Introducción
+Las redes neuronales convolucionales están _inspiradas_ en el funcionamiento 
+de la percepción de la visión:
+"""
+
+# ╔═╡ a6aa59f8-f25f-402f-a9e7-9553235f7303
+Resource(
+	"https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/funcionamiento_vision.png",
+	:alt => "Esquema del funcionamiento de la visión.",
+	:width => 900
+)
+
+# ╔═╡ ee29cc12-7007-4f16-88a0-1dd4ed894e78
+md"""
+## Introducción
+
+Las redes convolucionales proponen una solución basada en dos elementos:
+
+1. Utilizar convoluciones para **aprender** las características relevantes de 
+las imágenes.
+1. Utilizar **pooling** para reducir el tamaño de las imágenes.
+
+Además, en muchos casos las últimas capas de una red 
+convolucional están formadas por capas densas.
+
+Esto permite utilizar redes neuronales en las tareas típicas de la visión por 
+computador.
+"""
+
+# ╔═╡ 4e1c05fb-ebf4-4397-9ceb-838586369223
+md"""
+## Introducción
+
+Las tareas típicas dentro de la visión por computador son:
+
+* Clasificación.
+* Ubicación.
+* Detección de objetos.
+* Seguimiento de objetos.
+* Segmentación semántica.
+"""
+
+# ╔═╡ 2bdcaa99-eae1-404e-8d8f-4e41cd949ed5
+Resource(
+	"https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/french-bulldog.jpg",
+	:alt => "Esquema del funcionamiento de la visión.",
+	:width => 300
+)
+
+# ╔═╡ 2c33ed80-ec71-4ab3-8e90-4970fd0227a7
+Resource(
+	"https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/gato.jpg",
+	:alt => "Esquema del funcionamiento de la visión.",
+	:width => 300
+)
+
+# ╔═╡ fc07197a-2197-4bc4-a7dd-09d4bc5ef12d
+md"""
+![](https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/gato.jpg width="300")
+"""
+
+# ╔═╡ 56c8c212-6892-43a2-a309-4bb7fc601f26
+md"""
+
+![](https://www3.uji.es/~belfern/Docencia/IR2130_imagenes/RedesNeuronales/french-buldog.jpg width="300")
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.61"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.11.5"
+manifest_format = "2.0"
+project_hash = "6d1b77f27e79835fc27b2d7e99ab8fcaf37aa976"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "6e1d2a35f2f90a4bc7c2ed98079b2ba09c35b83a"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.3.2"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.2"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+version = "1.11.0"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+version = "1.11.0"
+
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.5"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.1.1+0"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+version = "1.11.0"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+version = "1.11.0"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "05882d6995ae5c12bb5f36dd2ed3f61c98cbb172"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.5"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "179267cfa5e712760cd43dcae385d7ea90cc25a4"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.5"
+
+[[deps.HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.5"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "b6d6bfdd7ce25b0f9b2f6b3dd56b2673a66c8770"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.5"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+version = "1.11.0"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.4"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.4"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "8.6.0+0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+version = "1.11.0"
+
+[[deps.LibGit2_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
+uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
+version = "1.7.2+0"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.11.0+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+version = "1.11.0"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+version = "1.11.0"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+version = "1.11.0"
+
+[[deps.MIMEs]]
+git-tree-sha1 = "c64d943587f7187e751162b3b84445bbbd79f691"
+uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
+version = "1.1.0"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+version = "1.11.0"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.6+0"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+version = "1.11.0"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2023.12.12"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.27+1"
+
+[[deps.Parsers]]
+deps = ["Dates", "PrecompileTools", "UUIDs"]
+git-tree-sha1 = "7d2f8f21da5db6a806faf7b9b292296da42b2810"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.8.3"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "Random", "SHA", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.11.0"
+
+    [deps.Pkg.extensions]
+    REPLExt = "REPL"
+
+    [deps.Pkg.weakdeps]
+    REPL = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
+git-tree-sha1 = "7e71a55b87222942f0f9337be62e26b1f103d3e4"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.61"
+
+[[deps.PrecompileTools]]
+deps = ["Preferences"]
+git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
+uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+version = "1.2.1"
+
+[[deps.Preferences]]
+deps = ["TOML"]
+git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
+uuid = "21216c6a-2e73-6563-6e65-726566657250"
+version = "1.4.3"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+version = "1.11.0"
+
+[[deps.Random]]
+deps = ["SHA"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+version = "1.11.0"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+version = "1.11.0"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "ae3bb1eb3bba077cd276bc5cfc337cc65c3075c0"
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.11.1"
+
+    [deps.Statistics.extensions]
+    SparseArraysExt = ["SparseArrays"]
+
+    [deps.Statistics.weakdeps]
+    SparseArrays = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+version = "1.11.0"
+
+[[deps.Tricks]]
+git-tree-sha1 = "6cae795a5a9313bbb4f60683f7263318fc7d1505"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.10"
+
+[[deps.URIs]]
+git-tree-sha1 = "cbbebadbcc76c5ca1cc4b4f3b0614b3e603b5000"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.5.2"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+version = "1.11.0"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+version = "1.11.0"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.13+1"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.11.0+0"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.59.0+0"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+2"
+"""
+
+# ╔═╡ Cell order:
+# ╠═17163d32-2fd3-11f0-053d-7b01c1dc1e5d
+# ╠═49bc4ae1-cc37-4de1-8616-d5bd2cc740ad
+# ╠═98925bf9-6cfa-4046-b0c7-de0a0ad286ad
+# ╠═bc7e1700-5d03-4493-84d7-b96a7da32d1b
+# ╠═c2db677d-1029-47ff-ac13-309d586ecc69
+# ╠═d8736fb8-a6a9-4e4d-bd28-509b2fdb6336
+# ╠═ce478b27-9250-41ff-898b-c887aa166ed3
+# ╠═7900f0f7-3ec0-434e-a6a8-2b6f381d3328
+# ╠═a0bbbc75-a852-47cf-88d5-982d03eb5c06
+# ╠═50a1c924-0a84-4cf0-99b7-476cb4b4afe7
+# ╠═8bdb7e0f-dece-4b09-a4e1-f32ee3f7601f
+# ╠═0dd08d60-2b9d-41d0-bcc8-927853f7c962
+# ╠═799ac9d4-2151-4474-a98e-1476e8bb1033
+# ╠═a6aa59f8-f25f-402f-a9e7-9553235f7303
+# ╠═ee29cc12-7007-4f16-88a0-1dd4ed894e78
+# ╠═4e1c05fb-ebf4-4397-9ceb-838586369223
+# ╠═2bdcaa99-eae1-404e-8d8f-4e41cd949ed5
+# ╠═2c33ed80-ec71-4ab3-8e90-4970fd0227a7
+# ╠═fc07197a-2197-4bc4-a7dd-09d4bc5ef12d
+# ╠═56c8c212-6892-43a2-a309-4bb7fc601f26
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
