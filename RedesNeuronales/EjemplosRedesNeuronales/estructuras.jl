22be906bd45273f9bf4151489b043e522d5c5a87
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2025-11-12 19:30:48 +0100

    Pequeño avance en la implementación de una red neuronal.

--- RedesNeuronales/EjemplosRedesNeuronales/estructuras.jl
+++ RedesNeuronales/EjemplosRedesNeuronales/estructuras.jl
@@ -13,17 +13,43 @@ mutable struct Capa
     nentradas::Int64
     nneuronas::Int64
     neuronas::Vector{Neurona}
-    activacion::Function
+    factivacion::Function
 
-    function Capa(nentradas::Int64, nneuronas::Int64, activacion::Function)::Capa
-        new(nentradas, nneuronas, [Neurona(nentradas) for _ in 1:nneuronas], activacion)
+    function Capa(nentradas::Int64, nneuronas::Int64, factivacion::Function)::Capa
+        new(nentradas, nneuronas, [Neurona(nentradas) for _ in 1:nneuronas], factivacion)
     end
 end
 
-function salida(entrada::Vector{Float64}, neurona::Neurona)
+mutable struct RedNeuronal
+    capas::Vector{Capa}
+    
+    function RedNeuronal(capas::Capa...)
+        new([c for c in capas])
+    end
+        
+end
+
+σ(x) = 1 / (1 + exp(-x))
+dσ(x) = σ(x)*(1 - σ(x))
+
+function activacion(entrada::Vector{Float64}, neurona::Neurona)
     (neurona.pesos * entrada)[1]
 end
 
 function salida(entrada::Vector{Float64}, capa::Capa)
-    [salida(entrada, neurona) for neurona in capa.neuronas]
+    [activacion(entrada, neurona) for neurona in capa.neuronas]
+end
+
+function forward(entrada::Vector{Float64}, red::RedNeuronal)
+    x = salida(entrada, red.capas[1])
+    for capa in red.capas[2:end]
+        x = salida(x, capa)
+    end
+    return x
+end
+
+function backprop(entrada::Vector{Float64}, red::RedNeuronal, y::Vector{Float64})
+    prediccion = forward(entrada, red)
+    error = prediccion - y
+    return error
 end
