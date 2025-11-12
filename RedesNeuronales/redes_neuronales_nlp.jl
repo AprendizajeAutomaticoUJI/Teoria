f9371501d05fa5e6020c7ba56e55c487f3583399
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2025-11-12 15:49:55 +0100

    Pequeños cambios para mostrar el código Julia.

--- RedesNeuronales/redes_neuronales_nlp.jl
+++ RedesNeuronales/redes_neuronales_nlp.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.8
+# v0.20.20
 
 using Markdown
 using InteractiveUtils
@@ -403,14 +403,17 @@ md"""
 
 En Julia podemos trabajar con embeddings utilizando el paquete **Embeddings**, que dispone de embeddings ya entrenados, algunos de ellos en español y catalán.
 
-```julia
+Para conocer los embbedings que tenemos disponibles:
+
+```..julia
 using Embeddings
 
-# Para conocer los embeddings que tenemos disponibles
 language_files(FastText_Text{:es})
 ```
 
-```shell
+En este caso se muestra la siguiente salida:
+
+```.shell
 4-element Vector{String}:
  "FastText es CommonCrawl Text/cc.es.300.vec"
  "FastText es Wiki Text/wiki.es.vec"
@@ -418,8 +421,9 @@ language_files(FastText_Text{:es})
  "FastText es Wiki Text/wiki.es.vec"
 ```
 
-```{.julia}
-# Para cargar el primero de los embeddings
+Para cargar el primero de los embeddings
+
+```.julia
 español = load_embeddings(FastText_Text{:es}, 1)
 ```
 """
@@ -430,7 +434,7 @@ md"""
 
 Para obtener el vector de embedding de una palabra en el diccionario puedes utilizar el código de ejemplo que encuentras en la documentación del paquete:
 
-```julia
+```.julia
 diccionario = Dict(palabra => indice for (indice, palabra) in enumerate(español.vocab))
 
 function obten_embedding(palabra)
@@ -450,7 +454,7 @@ md"""
 ## Show me the code
 Vamos a calcular el concepto de reina a partir de los conceptos mujer, rey, hombre, y calcular la distancia coseno entre el vector del embedding para reina y el calculado:
 
-```julia
+```.julia
 rey = obten_embedding("rey")
 reina = obten_embedding("reina")
 hombre = obten_embedding("hombre")
@@ -464,7 +468,7 @@ coseno(rey, rey2)
 
 Obtenemos:
 
-```shell
+```.shell
 0.7006942f0 (45 grados)
 0.6734288f0 (47 grados)
 ```
@@ -475,7 +479,7 @@ md"""
 ## Show me the code
 Vamos a calcular el concepto de gatos a partir de los conceptos perro, perros y gato, y calcular la distancia coseno entre el vector del embedding para gatos y el calculado:
 
-```julia
+```.julia
 perro = obten_embedding("perro")
 gato = obten_embedding("gato")
 perros = obten_embedding("perros")
@@ -487,7 +491,7 @@ coseno(gatos, gatos2)
 
 Obtenemos:
 
-```shell
+```.shell
 0.88137954f0 (28 grados)
 ```
 """
@@ -498,7 +502,7 @@ md"""
 
 Finalmente vamos a calcular el concepto de pluralidad a partir de perros y gatos, y compararlos:
 
-```julia
+```.julia
 pluralidad_gato = gatos - gato
 pluralidad_perro = perros - perro
 
@@ -507,7 +511,7 @@ coseno(pluralidad_gato, pluralidad_perro)
 
 Obtenemos:
 
-```shell
+```.shell
 0.86211735f0 (30 grados)
 ```
 """
@@ -562,7 +566,7 @@ md"""
 
 Crear una red para generar texto con GRU es relativamente sencillo:
 
-```julia
+```.julia
 modelo = Chain(
 		GRU(entrada => 128),
 		GRU(128 => 64),
@@ -575,7 +579,7 @@ modelo = Chain(
 !!! important "Detalle muy importante"
 	En la función de pérdidas debemos resetear el modelo antes de calcular las pérdidas, como en el siguiente fragmento de código:
 	
-	```julia
+	```.julia
 	function perdidas(modelo, X, y)
 		Flux.reset!(modelo)
 		Flux.Losses.crossentropy(modelo(X), y)
@@ -589,7 +593,7 @@ md"""
 
 El siguiente paso es entrenar la red:
 
-```julia
+```.julia
 function entrena_modelo(modelo, Xtrain, ytrain, λ, epocas)
 	datos = [(Xtrain, ytrain)] # Preparamos los datos
     optimizador = Flux.setup(Adam(λ), modelo) # Instanciamos el optimizador
@@ -622,13 +626,13 @@ Además, cuando el conjunto de entrenamiento es muy grande (muchos texto o texto
 
 Para usar la tarjeta gráfica de nuestro equipo debemos utilizar la biblioteca CUDA:
 
-```julia
+```.julia
 using CUDA
 ```
 
 Una vez creados el modelo lo enviamos a la tarjeta:
 
-```julia
+```.julia
 modelo = Chain(
 		GRU(entrada => 128),
 		GRU(128 => 64),
@@ -653,7 +657,7 @@ md"""
 
 El último paso es indicar que el entrenamiento se va a hacer por lotes:
 
-```julia
+```.julia
 function entrena_modelo(modelo, Xtrain, ytrain, λ, epocas, tamaño_lote)
     lotes = Flux.DataLoader((Xtrain, ytrain), shuffle = true, batchsize = tamaño_lote)
     optimizador = Flux.setup(Adam(λ), modelo)
@@ -700,7 +704,7 @@ Resource(
 md"""
 Supongamos que queremos clasificar textos como positivos, negativos o neutros.
 
-En el conjunto de entrenamiento, o todos los textos tienen el mismo número de palabras.
+En el conjunto de entrenamiento, no todos los textos tienen el mismo número de palabras.
 """
 
 # ╔═╡ a7b091ab-0dcc-44c9-a93e-118d1610a855
@@ -1114,9 +1118,9 @@ TextAnalysis = "~0.8.2"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.11.7"
+julia_version = "1.12.1"
 manifest_format = "2.0"
-project_hash = "afc6e5481b635b74bcf8a71383dcb1a8dd6b5eec"
+project_hash = "469d671dabd4e110c5c3a2b34d27fb2bc4bc0db1"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -1158,12 +1162,10 @@ deps = ["FixedPointNumbers", "Random"]
 git-tree-sha1 = "67e11ee83a43eb71ddc950302c53bf33f0690dfe"
 uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
 version = "0.12.1"
+weakdeps = ["StyledStrings"]
 
     [deps.ColorTypes.extensions]
     StyledStringsExt = "StyledStrings"
-
-    [deps.ColorTypes.weakdeps]
-    StyledStrings = "f489334b-da3d-4c2e-b8f0-e476e12c162b"
 
 [[deps.Compat]]
 deps = ["TOML", "UUIDs"]
@@ -1178,7 +1180,7 @@ weakdeps = ["Dates", "LinearAlgebra"]
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "1.1.1+0"
+version = "1.3.0+1"
 
 [[deps.ConcurrentUtilities]]
 deps = ["Serialization", "Sockets"]
@@ -1319,6 +1321,11 @@ version = "1.14.3"
     [deps.JSON3.weakdeps]
     ArrowTypes = "31f734f8-188a-4ce0-8406-c8a06bd891cd"
 
+[[deps.JuliaSyntaxHighlighting]]
+deps = ["StyledStrings"]
+uuid = "ac6e5ff7-fb65-4e79-a425-ec3bc9c03011"
+version = "1.12.0"
+
 [[deps.Languages]]
 deps = ["InteractiveUtils", "JSON", "RelocatableFolders"]
 git-tree-sha1 = "0cf92ba8402f94c9f4db0ec156888ee8d299fcb8"
@@ -1331,24 +1338,24 @@ uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
 version = "0.6.4"
 
 [[deps.LibCURL_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "OpenSSL_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "8.6.0+0"
+version = "8.11.1+1"
 
 [[deps.LibGit2]]
-deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+deps = ["LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
 uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 version = "1.11.0"
 
 [[deps.LibGit2_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "OpenSSL_jll"]
 uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
-version = "1.7.2+0"
+version = "1.9.0+0"
 
 [[deps.LibSSH2_jll]]
-deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+deps = ["Artifacts", "Libdl", "OpenSSL_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.11.0+1"
+version = "1.11.3+1"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -1357,7 +1364,7 @@ version = "1.11.0"
 [[deps.LinearAlgebra]]
 deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
-version = "1.11.0"
+version = "1.12.0"
 
 [[deps.LogExpFunctions]]
 deps = ["DocStringExtensions", "IrrationalConstants", "LinearAlgebra"]
@@ -1396,7 +1403,7 @@ uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
 version = "0.5.16"
 
 [[deps.Markdown]]
-deps = ["Base64"]
+deps = ["Base64", "JuliaSyntaxHighlighting", "StyledStrings"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 version = "1.11.0"
 
@@ -1407,7 +1414,8 @@ uuid = "739be429-bea8-5141-9913-cc70e7f3736d"
 version = "1.1.9"
 
 [[deps.MbedTLS_jll]]
-deps = ["Artifacts", "Libdl"]
+deps = ["Artifacts", "JLLWrappers", "Libdl"]
+git-tree-sha1 = "926c6af3a037c68d02596a44c22ec3595f5f760b"
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
 version = "2.28.6+0"
 
@@ -1429,16 +1437,16 @@ version = "1.11.0"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2023.12.12"
+version = "2025.5.20"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
+version = "1.3.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.27+1"
+version = "0.3.29+0"
 
 [[deps.OpenSSL]]
 deps = ["BitFlags", "Dates", "MozillaCACerts_jll", "OpenSSL_jll", "Sockets"]
@@ -1447,10 +1455,9 @@ uuid = "4d8831e6-92b7-49fb-bdf8-b643e874388c"
 version = "1.5.0"
 
 [[deps.OpenSSL_jll]]
-deps = ["Artifacts", "JLLWrappers", "Libdl"]
-git-tree-sha1 = "2ae7d4ddec2e13ad3bddf5c0796f7547cf682391"
+deps = ["Artifacts", "Libdl"]
 uuid = "458c3c95-2e84-50aa-8efc-19380b2a3a95"
-version = "3.5.2+0"
+version = "3.5.1+0"
 
 [[deps.OrderedCollections]]
 git-tree-sha1 = "05868e21324cede2207c6f0f466b4bfef6d5e7ee"
@@ -1466,7 +1473,7 @@ version = "2.8.3"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "Random", "SHA", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.11.0"
+version = "1.12.0"
 
     [deps.Pkg.extensions]
     REPLExt = "REPL"
@@ -1574,7 +1581,7 @@ version = "1.2.2"
 [[deps.SparseArrays]]
 deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
-version = "1.11.0"
+version = "1.12.0"
 
 [[deps.Statistics]]
 deps = ["LinearAlgebra"]
@@ -1610,10 +1617,14 @@ git-tree-sha1 = "159331b30e94d7b11379037feeb9b690950cace8"
 uuid = "856f2bd8-1eba-4b0a-8007-ebc267875bd4"
 version = "1.11.0"
 
+[[deps.StyledStrings]]
+uuid = "f489334b-da3d-4c2e-b8f0-e476e12c162b"
+version = "1.11.0"
+
 [[deps.SuiteSparse_jll]]
 deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
 uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
-version = "7.7.0+0"
+version = "7.8.3+2"
 
 [[deps.TOML]]
 deps = ["Dates"]
@@ -1681,22 +1692,22 @@ version = "0.5.6"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.13+1"
+version = "1.3.1+2"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.11.0+0"
+version = "5.15.0+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.59.0+0"
+version = "1.64.0+1"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+2"
+version = "17.5.0+2"
 """
 
 # ╔═╡ Cell order:
